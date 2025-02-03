0eacd044a25701b550bd9f00da66c77a133aa5c5
premnath018 <prem185nath@gmail.com>
2025-02-03 12:45:08 +0530

    Modified course resource

--- app/Filament/Resources/CourseResource.php
+++ app/Filament/Resources/CourseResource.php
@@ -64,7 +64,7 @@ class CourseResource extends Resource
                                     $set('course_meta_description', $state);
                                 }
                             }),
-                        Select::make('class_level_id')
+                        Select::make('class_level')
                             ->label('Class Level')
                             ->relationship('classLevel', 'name')
                             ->native(false)
